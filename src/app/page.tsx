7ef983597820ea76bf61a1d09bbbdc4fc5cafde7
Karl J. Overå <k@rl.run>
2023-03-24 20:09:16 +0100

    add cron job to revalidate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
 import { Inter } from 'next/font/google';
-import dynamic from "next/dynamic";
+import dynamic from 'next/dynamic';
 
 import { getTimes } from '@/scraping/obf';
 import { cn } from '@/utils/cn';
 
-const LastUpdated = dynamic(() => import('@/components/LastUpdated'), { ssr: false });
+const LastUpdated = dynamic(() => import('@/components/LastUpdated'), {
+  ssr: false,
+  loading: () => <p>Klokkeslettene</p>,
+});
 
 const inter = Inter({ subsets: ['latin'] });
-
-// let cache: { dayInMonth: number; weekday: string; times: Record<string, number> }[] | null = null;
 
 export const revalidate = 60;
 
 export default async function Home() {
-  /*
   let times: { dayInMonth: number; weekday: string; times: Record<string, number> }[];
-  if (global.cache == null) {
-    times = await getTimes();
-    global.cache = times;
+  if (process.env.NODE_ENV === 'development') {
+    if ((global as any).cache == null) {
+      times = await getTimes();
+      (global as any).cache = times;
+    } else {
+      console.info('Using cached times');
+      times = (global as any).cache;
+    }
   } else {
-    console.info('Using cached times');
-    times = global.cache;
+    times = await getTimes();
   }
-  */
-
-  const times = await getTimes();
 
   return (
     <main className={cn(inter.className, 'p-4 sm:p-16')}>
@@ -32,9 +33,9 @@ export default async function Home() {
         <h1 className="text-2xl font-bold">Hemmelig Kroloftet Booking Oversikt</h1>
         <LastUpdated generatedAt={new Date().toISOString()} />
       </div>
-      <div className="flex flex-wrap gap-4 ">
+      <div className="flex flex-wrap justify-between gap-4">
         {times.map((it) => (
-          <div key={it.dayInMonth} className="min-w-[12rem] flex-grow bg-slate-100 p-4 sm:flex-grow-0">
+          <div key={it.dayInMonth} className="min-w-[12rem] flex-grow bg-slate-100 p-4">
             <h2 className="text-md font-bold">
               {it.dayInMonth}: {it.weekday}
             </h2>
