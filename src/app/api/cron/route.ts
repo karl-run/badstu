1d977860443cc5c8468ebccf3c42c16ad6ba0bc8
Karl J. Overå <k@rl.run>
2023-03-31 08:49:49 +0200

    fix cron

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,29 +1,22 @@
-import * as E from 'fp-ts/Either';
-import * as TE from 'fp-ts/TaskEither';
-import { pipe } from 'fp-ts/function';
-
 const HOST = `https://${process.env.VERCEL_URL}` ?? 'http://localhost:3000';
 
-export const GET = async (request: Request): Promise<Response> =>
-  pipe(
-    eitherFetch(HOST + '/api/scrape?source=cron', { method: 'POST' }),
-    TE.chain(() => eitherFetch(HOST + '/api/revalidate', { method: 'POST' })),
-    TE.mapLeft(toErrorResponse),
-    TE.map(toSuccessResponse),
-    TE.toUnion,
-  )();
+export const GET = async (request: Request): Promise<Response> => {
+  try {
+    await throwFetch(HOST + '/api/scrape?source=cron', { method: 'POST' });
+    await throwFetch(HOST + '/api/revalidate', { method: 'POST' });
 
-const eitherFetch = (...args: Parameters<typeof fetch>): TE.TaskEither<Error, Response> =>
-  TE.tryCatch(async () => {
-    console.log('Fetching', args[0]);
-    const response = await fetch(...args);
+    return new Response('OK', { status: 200 });
+  } catch (e) {
+    console.error(e);
+    return new Response('Error', { status: 500 });
+  }
+};
+
+const throwFetch = (...args: Parameters<typeof fetch>): Promise<Response> =>
+  fetch(...args).then((response) => {
     if (response.ok) {
       return response;
     } else {
       throw new Error(`Request failed with status ${response.status}`);
     }
-  }, E.toError);
-
-const toErrorResponse = (error: Error) => new Response(error.message ?? 'No idea', { status: 500 });
-
-const toSuccessResponse = (response: Response) => new Response('OK', { status: response.status });
+  });
