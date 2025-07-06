44f07731304bb327bc01bcfbd9e6be293f4e5b62
Karl J. Overå <k@rl.run>
2025-07-06 21:19:19 +0200

    chore: basic web layout, bump all deps

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -4,8 +4,6 @@ import { getLock } from '@badstu/db/lock'
 export const load: PageServerLoad = async () => {
   const rows = await getLock('kroloftet')
 
-  console.log(rows)
-
   return {
     serverMessage: 'hello from server load function',
     rows,
