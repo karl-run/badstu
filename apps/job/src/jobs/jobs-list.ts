e37efac7f8f756d8bccf09f7b268d083cacc3271
Karl J. Overå <k@rl.run>
2025-07-13 14:57:01 +0200

    feat: scrape other variations at Kroloftet, dont save 0-avail slots

--- apps/job/src/jobs/jobs-list.ts
+++ apps/job/src/jobs/jobs-list.ts
@@ -2,7 +2,11 @@ import { ObfJob } from './common'
 import { obfLocations } from '@badstu/data'
 
 export const obfJobs = [
-  new ObfJob('Svarttrosten', obfLocations.kroloftet),
+  new ObfJob('Svarttrosten', obfLocations.kroloftet.svarttrosten),
+  new ObfJob('Svarttrosten nakenbadstue', obfLocations.kroloftet['svarttrosten nakenbadstue']),
+  new ObfJob('Svarttrosten nakenbadstue (kvinner)', obfLocations.kroloftet['svarttrosten nakenbadsue (kvinner)']),
+  new ObfJob('Jurten', obfLocations.kroloftet.jurten),
+  new ObfJob('Svarttrosten', obfLocations.kroloftet.svarttrosten),
   new ObfJob('Langkaia', obfLocations.langkaia),
   new ObfJob('Sukkerbiten', obfLocations.sukkerbiten),
   new ObfJob('Sagene med basseng', obfLocations.sagene['badstu/bad u/ nakenhet']),
