d094062c38762df2f2f513dc516126f848a3bb28
Karl J. Overå <k@rl.run>
2025-07-13 22:08:49 +0200

    feat: actually work on mobile

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -10,7 +10,7 @@
 
 <header class="flex h-16 items-center justify-between border-b border-b-gray-300 bg-gray-100">
   <a href="/" class="group flex items-center gap-4">
-    <img src={logo} alt="" height={32} width={32} class="ml-4 hidden h-10 w-10 shrink-0 sm:block" />
+    <img src={logo} alt="" height={32} width={32} class="ml-4 h-10 w-10 shrink-0" />
     <h1 class="flex items-center gap-1 text-2xl text-gray-800">
       <ArrowLeft class={['transition-all group-hover:-translate-x-1', page.url.pathname === '/' && 'w-0']} />
       <span>Badstu i Oslo</span>
--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -21,58 +21,62 @@
   let { data }: PageProps = $props()
 </script>
 
-<div class="container p-4">
-  <h2 class="mb-3 ml-4 text-xl">Badstuer i dag</h2>
-  <div class="flex gap-3 overflow-hidden">
-    {#each R.entries(data.locations) as [name, location]}
-      <div
-        class="relative flex w-[calc(100vw-100px)] grow flex-col overflow-hidden rounded-2xl bg-gray-200 md:w-64 md:grow"
-      >
-        <a class="relative h-18 bg-black hover:bg-blue-100" href="/badstu/{name.replaceAll(' ', '-')}">
-          <BadstuCovers
-            location={name}
-            class="pointer-events-none absolute inset-0 top-0 left-0  h-full w-full opacity-70"
-          />
-          <h2
-            class="flex h-full w-full items-center justify-center text-xl font-bold text-white drop-shadow-md drop-shadow-black"
-          >
-            {name}
-          </h2>
-        </a>
-        <div>
-          {#if location.slots.length > 0}
-            <div class="divide-y divide-gray-400">
-              {#each location.slots as slot}
-                <RowLink {location} {slot} />
-              {/each}
+<div class="grid h-[calc(100vh-4rem)] grid-cols-1 grid-rows-[calc(70vh-4rem)_30vh]">
+  <div class="flex flex-col">
+    <h2 class="p-4 text-xl">Badstuer i dag</h2>
+    <div class="flex max-w-screen gap-3 overflow-scroll px-3 pb-3">
+      {#each R.entries(data.locations) as [name, location]}
+        <div class="relative w-full max-w-[calc(100vw-100px)] min-w-72 grow rounded-2xl bg-gray-200 md:w-64 md:grow">
+          <div class="flex h-full flex-col rounded-2xl">
+            <a class="group relative h-18 shrink-0" href="/badstu/{name.replaceAll(' ', '-')}">
+              <BadstuCovers
+                location={name}
+                class="pointer-events-none absolute inset-0 top-0 left-0 h-full w-full rounded-t-2xl opacity-70 group-hover:opacity-100"
+              />
+              <h2
+                class="flex h-full w-full items-center justify-center text-xl font-bold text-white drop-shadow-md drop-shadow-black"
+              >
+                {name}
+              </h2>
+            </a>
+            <div class="overflow-y-scroll">
+              {#if location.slots.length > 0}
+                <div class="divide-y divide-gray-400">
+                  {#each location.slots as slot}
+                    <RowLink {location} {slot} />
+                  {/each}
+                </div>
+              {:else}
+                <div class="no-slots">Ingen ledige</div>
+              {/if}
             </div>
-          {:else}
-            <div class="no-slots">Ingen ledige</div>
-          {/if}
-        </div>
-        <div class="grow"></div>
-        <div class="flex justify-between">
-          <button
-            class="m-1 ml-2 cursor-pointer rounded-full p-2 hover:shadow-2xl hover:outline-1"
-            onclick={() => {
-              const location = allBadstuLocations[name]
-              if (!location || !location.loc) {
-                console.error(`No location found for ${name}`)
-                return
-              }
+            <div class="grow"></div>
+            <div class="flex justify-between">
+              <button
+                class="m-1 ml-2 cursor-pointer rounded-full p-2 hover:shadow-2xl hover:outline-1"
+                onclick={() => {
+                  const location = allBadstuLocations[name]
+                  if (!location || !location.loc) {
+                    console.error(`No location found for ${name}`)
+                    return
+                  }
 
-              return map?.flyTo({ center: location.loc, zoom: 15 })
-            }}
-          >
-            <LocateFixed aria-label="zoom to location" class="h-5 w-5" />
-          </button>
-          <a class="mr-2 flex items-center gap-1" href="/badstu/{name.replaceAll(' ', '-')}"
-            >Andre dager <ArrowRight class="h-4 w-4" /></a
-          >
+                  return map?.flyTo({ center: location.loc, zoom: 15 })
+                }}
+              >
+                <LocateFixed aria-label="zoom to location" class="h-5 w-5" />
+              </button>
+              <a class="mr-2 flex items-center gap-1" href="/badstu/{name.replaceAll(' ', '-')}"
+                >Andre dager <ArrowRight class="h-4 w-4" /></a
+              >
+            </div>
+          </div>
         </div>
-      </div>
-    {/each}
+      {/each}
+    </div>
+  </div>
+
+  <div>
+    <BadstuMap class="h-full w-full" />
   </div>
 </div>
-
-<BadstuMap />
