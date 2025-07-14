c5b107d094a196c10e5d99ee457c235d0385369c
Karl J. Overå <k@rl.run>
2025-07-14 21:18:52 +0200

    perf: use @sveltejs/enhanced-img for logo

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import '../app.css'
   import { ArrowLeft } from '@lucide/svelte'
-  import logo from '$lib/assets/logo.png'
+  import logo from '$lib/assets/logo.png?enhanced'
 
   import { page } from '$app/state'
 
@@ -10,7 +10,9 @@
 
 <header class="flex h-16 items-center justify-between border-b border-b-gray-300 bg-gray-100">
   <a href="/" class="group flex items-center gap-4">
-    <img src={logo} alt="" height={32} width={32} class="ml-4 h-10 w-10 shrink-0" />
+    <div class="ml-4 h-10 w-10 shrink-0">
+      <enhanced:img src={logo} />
+    </div>
     <h1 class="flex items-center gap-1 text-2xl text-gray-800">
       <ArrowLeft class={['transition-all group-hover:-translate-x-1', page.url.pathname === '/' && 'w-0']} />
       <span>Badstu i Oslo</span>
--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,14 @@
 import devtoolsJson from 'vite-plugin-devtools-json'
 import tailwindcss from '@tailwindcss/vite'
 import { sveltekit } from '@sveltejs/kit/vite'
+import { enhancedImages } from '@sveltejs/enhanced-img'
 import { defineConfig, loadEnv } from 'vite'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
 
   return {
-    plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+    plugins: [tailwindcss(), sveltekit(), enhancedImages(), devtoolsJson()],
     define: {
       'process.env.TURSO_DATABASE_URL': JSON.stringify(env.TURSO_DATABASE_URL),
       'process.env.TURSO_AUTH_TOKEN': JSON.stringify(env.TURSO_AUTH_TOKEN),
