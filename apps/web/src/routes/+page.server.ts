e29cbd627210a5ccf8f390f9c60808a757436e99
Karl J. Overå <k@rl.run>
2025-09-01 21:11:30 +0200

    perf: 🤦

--- apps/web/src/routes/+page.server.ts
+++ apps/web/src/routes/+page.server.ts
@@ -9,8 +9,6 @@ export const load: PageServerLoad = async ({ depends }) => {
 
   logger.info("Landing page, generating today's availability")
   const locations = getAllAvailabilityForDate(new Date()).then(async (it) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000)) // Simulate delay for testing purposes
-
     const endTime = Date.now()
     logger.info(`Today's availability generated for ${Object.keys(it).length} locations, took ${endTime - startTime}ms`)
     return it
