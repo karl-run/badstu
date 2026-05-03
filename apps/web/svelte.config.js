dd889b349e6a97e16ef7311e2f731ab097638b3f
Karl J. Overå <k@rl.run>
2026-05-03 10:52:41 +0200

    chore: cloudflare'd up

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -1,10 +1,18 @@
-import adapter from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: adapter({
+      fallback: 'plaintext',
+      routes: {
+        include: ['/*'],
+        exclude: ['<all>'],
+      },
+    }),
+  },
 }
 
 export default config
